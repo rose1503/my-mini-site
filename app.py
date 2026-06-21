3f0611d9f628f4ff257cb9106faefb31e45e648f
Rose <kamdoum.rosine@yahoo.fr>
2026-06-20 22:27:58 -0400

    update hompage text

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hello from my CI/CD website!</h1>"
+    return "<h1>Hello from my CI/CD website! Now updated</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
